7612648259b012e5194d866e1f7a784394c453bb
Dijar06 <dize06001@utb.vaxjo.se>
2024-02-02 12:30:27 +0100

    Add project files.

---
+++ Uppgift 7.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namn = new List<string>();
+            Console.WriteLine("Skriv hur många namn som helst, avsluta med tom rad");
+            string nynamn = " ";
+            while (nynamn != "")
+            {
+                Console.WriteLine("Skriv nytt namn:");
+                nynamn = Console.ReadLine();
+                if (nynamn != "")
+                {
+                    namn.Add(nynamn);
+                }
+            }
+            Console.WriteLine("Här är alla namn du skrev in:");
+            namn.Sort();
+            namn.Reverse();
+            foreach (string element in namn)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
